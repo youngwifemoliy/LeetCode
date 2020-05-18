7010bd95deb33962845c9309e5923f6ad1e6fa05
林善伦 <youngwifemoliy@live.cn>
2020-05-18 15:11:10 +0800

    no message

--- 探索础计算法/3.无重复字符的最长子串.js
+++ 探索础计算法/3.无重复字符的最长子串.js
@@ -11,7 +11,25 @@
  */
 //slidng window
 var lengthOfLongestSubstring = function (s) {
-
+    let set = new Set(),
+        i = 0,
+        j = 0,
+        maxCount = 0;
+    for (i; i < s.length; i++) {
+        if (!set.has(s[i])) {
+            set.add(s[i]);
+            maxCount = Math.max(maxCount, set.size);
+        } else {
+            while (set.has(s[i])) {
+                set.delete(s[j]);
+                j++;
+            }
+            set.add(s[i]);
+        }
+    }
+    console.log(maxCount);
+    return maxCount;
 };
-// @lc code=end
 
+
+// @lc code=end
