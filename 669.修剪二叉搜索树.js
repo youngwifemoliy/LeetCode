2735b172b26a9a8724070c82708abb42c67f9d07
林善伦 <youngwifemoliy@live.cn>
2020-05-19 15:02:20 +0800

    调整结构

---
+++ 669.修剪二叉搜索树.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=669 lang=javascript
+ *
+ * [669] 修剪二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {TreeNode}
+ */
+var trimBST = function(root, L, R) {
+
+};
+// @lc code=end
+
