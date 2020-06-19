f1d347324034f5245221449cef9c9e752ae9206e
林善伦 <youngwifemoliy@live.cn>
2020-06-19 15:42:00 +0800

    no message

---
+++ 2.两数相加.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=javascript
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+    let stack1 = [],
+        stack2 = [];
+    while (l1) {
+        stack1.push(l1.val);
+        l1 = l1.next;
+    }
+    while (l1) {
+        stack1.push(l1.val);
+        l1 = l1.next;
+    }
+};
+addTwoNumbers()
+// @lc code=end
