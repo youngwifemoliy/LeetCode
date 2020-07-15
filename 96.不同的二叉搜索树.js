c5ee96c8fcee13ee6f5885db6a1b8a73eaac3f4d
林善伦 <youngwifemoliy@live.cn>
2020-07-15 10:15:45 +0800

    1

---
+++ 96.不同的二叉搜索树.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=96 lang=javascript
+ *
+ * [96] 不同的二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numTrees = function (n) {
+    const G = new Array(n + 1).fill(0);
+    G[0] = 1;
+    G[1] = 1;
+
+    for (let i = 2; i <= n; ++i) {
+        for (let j = 1; j <= i; ++j) {
+            G[i] += G[j - 1] * G[i - j];
+        }
+    }
+    return G[n];
+};
+// @lc code=end
+
